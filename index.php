6917c8ca133db288040b5e0779b6260242d92c9c
Owner <Owner@MarkVanWeelden>
2014-01-18 19:07:35 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form name="wishlist" action="wishlist.php">
+            Show wish list of: <input type="text" name="user" value="" />
+            <input type="submit" value="Go" />
+            <br>Still don't have a wish list?! <a href="createNewWisher.php">Create now</a>
+        </form>
+    </body>
+</html>
